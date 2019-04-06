feee93409317339c64d71f00fb39c3cb2c0556c3
AliAhmed15 <49337548+AliAhmed15@users.noreply.github.com>
2019-04-06 15:02:09 +0500

    Add files via upload
    lab1 tasks oop

---
+++ Task1.java
@@ -0,0 +1,21 @@
+class Task1{
+	public static void main(String args []){
+
+          String name="Ali Ahmed Chohan";
+          String rollno="18SW07";
+          int totalmarks=818;
+          char grade='A';
+          double percentage=74.36;
+          boolean status=true;
+
+          System.out.println(name);
+          System.out.println(rollno);
+          System.out.println(totalmarks);
+          System.out.println(grade);
+          System.out.println(percentage);
+          System.out.println(status);
+
+
+
+	}
+}
---
+++ Task2.java
@@ -0,0 +1,14 @@
+class Task2{
+	public static void main(String args []){
+     double d=50.25;
+     System.out.println(d);
+     int a=2;
+     a=(int)d;
+     System.out.println(a);
+     System.out.println(d-a);
+
+
+
+
+	}
+	}
